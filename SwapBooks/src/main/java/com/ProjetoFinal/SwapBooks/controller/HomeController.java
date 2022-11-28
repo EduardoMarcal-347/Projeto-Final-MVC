bae361e94216e88822fe302123cde2062379b2a6
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-11-28 02:04:04 -0300

    criacao template e outras telas

---
+++ SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ProjetoFinal.SwapBooks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
