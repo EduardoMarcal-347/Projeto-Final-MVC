96c43464d771245aa8ed2d3e6bd09a5142b22bb6
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-12-08 19:41:46 -0300

    commit repository

--- SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/repository/LivroRepository.java
+++ SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/repository/LivroRepository.java
@@ -1,5 +1,12 @@
 package com.ProjetoFinal.SwapBooks.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LivroRepository {
+    @Autowired
+    JdbcTemplate jdbc;
     
 }
--- SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/repository/UsuarioRepository.java
+++ SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/repository/UsuarioRepository.java
@@ -1,5 +1,14 @@
 package com.ProjetoFinal.SwapBooks.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UsuarioRepository {
+    @Autowired
+    JdbcTemplate jdbc;
+
+    
     
 }
