bf6698b2972267c5fea573da5bd835b65afa3d17
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2022-12-08 19:47:57 -0300

    applicationProperties

--- SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/repository/UsuarioRepository.java
+++ SwapBooks/src/main/java/com/ProjetoFinal/SwapBooks/repository/UsuarioRepository.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.ProjetoFinal.SwapBooks.model.Usuario;
+
 @Repository
 public class UsuarioRepository {
     @Autowired
     JdbcTemplate jdbc;
 
-    
+    public void save(Usuario usuario) {
+        String sql = "insert into su"
+    }
     
 }
